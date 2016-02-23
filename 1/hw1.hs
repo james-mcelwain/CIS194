9de179602e3ac240c025dec0c6e708ce57662bd9
James McElwain <jcm@Jamess-MacBook-Pro.local>
2016-02-22 18:24:34 -0600

    init w/ h1 started

---
+++ 1/hw1.hs
@@ -0,0 +1,54 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW01 where
+
+-- Exercise 1 -----------------------------------------
+
+-- Get the last digit from a number
+lastDigit :: Integer -> Integer
+lastDigit x = mod x 10
+
+
+-- Drop the last digit from a number
+dropLastDigit :: Integer -> Integer
+dropLastDigit x = div (x - mod x 10) 10
+
+toDigits :: Integer -> [Integer]
+toDigits x
+  | x <= 0    = []
+  | otherwise = toDigits (div x 10) ++ [dropLastDigit x]
+
+-- Exercise 2 -----------------------------------------
+
+toRevDigits :: Integer -> [Integer]
+toRevDigits x
+  | x <= 0    = []
+  | otherwise = lastDigit x : toDigits (dropLastDigit x)
+
+
+-- Exercise 3 -----------------------------------------
+
+-- Double every second number in a list starting on the left.
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = undefined
+
+-- Exercise 4 -----------------------------------------
+
+-- Calculate the sum of all the digits in every Integer.
+sumDigits :: [Integer] -> Integer
+sumDigits = undefined
+
+
+-- Exercise 5 -----------------------------------------
+
+-- Validate a credit card number using the above functions.
+luhn :: Integer -> Bool
+luhn = undefined
+
+-- Exercise 6 -----------------------------------------
+
+-- Towers of Hanoi for three pegs
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi = undefined
